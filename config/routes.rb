ef5febe5aa307bf01bef6891f23c1ea6b1894552
Justin Slama <jslama@shaw.ca>
2019-10-30 00:14:50 -0500

    Added characters controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'characters/index'
+  get 'characters/show'
   get 'planets/index'
   get 'planets/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
