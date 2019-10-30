d58472b44b59afbace5e9b5425c2af7b2f927b33
Justin Slama <jslama@shaw.ca>
2019-10-30 00:23:49 -0500

    Updated controller routes and set root to planets

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  get 'starships/index'
-  get 'starships/show'
-  get 'species/index'
-  get 'species/show'
-  get 'characters/index'
-  get 'characters/show'
-  get 'planets/index'
-  get 'planets/show'
+  get '/starships', to: 'starships#index'
+  get '/starships/:id', to: 'starships#show', id: /\d+/
+
+  get '/species', to: 'species#index'
+  get '/species/:id', to: 'species#show', id: /\d+/
+
+  get '/characters', to: 'characters#index'
+  get '/characters/:id', to: 'characters#show', id: /\d+/
+
+  get '/planets', to: 'planets#index'
+  get '/planets/:id', to: 'planets#show', id: /\d+/
+
+  root to: 'planets#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
