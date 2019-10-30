aa67494a6fc329e1cc35092145aade69461b05ea
Justin Slama <jslama@shaw.ca>
2019-10-30 00:11:33 -0500

    Added validation and has_many relationships to character model

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,10 @@
+# frozen_string_literal: true
+
 class Character < ApplicationRecord
   belongs_to :planet
+
+  has_many :character_species
+  has_many :character_starships
+
+  validates :name, presence: true
 end
